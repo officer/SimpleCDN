7f591fd75f808b02aa1fcb445338504b887e8e12
officer <kasaiso@amazon.com>
2019-09-01 21:11:45 +0100

    remove domain since it won't be used

--- infrastructure/domain/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route53_zone" "domain" {
-  lifecycle { 
-    prevent_destroy = false
-  }
-  
-  name = "${var.domain_name}"
-  tags = "${merge(var.tags, map("Domain", var.domain_name))}"
-  force_destroy = true
-}
--- infrastructure/domain/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "zone_id" {
-  value = "${aws_route53_zone.domain.zone_id}"
-}
-
-output "name_servers" {
-  value = "${aws_route53_zone.domain.name_servers}"
-}
-
